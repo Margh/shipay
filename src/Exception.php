4275e6008e8740b828c3bf97ed4f1bba09fae803
victor <victordeaguiarsouza@gmail.com>
2020-11-26 09:16:04 -0300

    retornando o código do erro na classe Shipay/Exception

--- src/Exception.php
+++ src/Exception.php
@@ -15,5 +15,6 @@ class Exception extends \Exception {
         $this->request = $request;
 
         $this->message = !isset($request->response->error)? $request->rawResponse : json_encode($request->response->error);
+        $this->code    = !isset($request->response->error)? $request->errorCode : 0;
     }
 }
